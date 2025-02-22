d273041e25328f7ec03021d3c85f63e62ed2f2f8
HIMANSHU SINGH <100193490+23himanshusingh@users.noreply.github.com>
2025-02-23 01:45:09 +0530

    added dummyLogin, Profile, and Navbar components to test routing; updated App routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,29 @@
-function App() {
-  return (
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from './Body.jsx';
+import Login from "./Login.jsx";
+import Profile from "./Profile.jsx";
+
+const root = document.getElementById("root");
+
+function App () {
+  return(  
     <>
-      <div className="navbar bg-base-300">
-        <div className="flex-1 mx-0.5">
-          <a className="btn btn-ghost text-xl">devTinder</a>
-        </div>
-        <div className="flex-none gap-2">
-          <div className="dropdown dropdown-end mx-4">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <h1 className="text-2xl font-bold">Hello World!</h1>
-    </>
-  );
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Body />}>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+</>)
 }
 
+
 export default App;
+
+
+
+
+
