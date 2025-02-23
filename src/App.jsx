d1652d11c4c6ac7cfd510a3682d80fbb0e228d96
HIMANSHU SINGH <100193490+23himanshusingh@users.noreply.github.com>
2025-02-23 13:06:14 +0530

    added axios for API calls in Login component; improved styling and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Body from './Body.jsx';
+import Body from "./Body.jsx";
 import Login from "./Login.jsx";
 import Profile from "./Profile.jsx";
 
 const root = document.getElementById("root");
 
-function App () {
-  return(  
+function App() {
+  return (
     <>
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Body />}>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
-</>)
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-
 export default App;
-
-
-
-
-
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+    const [email, setEmail] = useState("singh@gmail.com");
+    const [password, setPassword] = useState("Singh@123");
+    const onClickHandle = async () => {
+        try{
+            const res = await axios.post("http://localhost:3000/login",{
+                email,
+                password
+            },{withCredentials:true});
+        }catch(err){
+            console.error(err);
+        }
+    };
   return (
-    <div>
-      Login
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">EmailId</span>
+            </div>
+            <input
+              type="text"
+              value={email}
+              className="input input-bordered w-full max-w-xs"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Password</span>
+            </div>
+            <input
+              type="text"
+              value={password}
+              className="input input-bordered w-full max-w-xs"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={onClickHandle}>Login</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
